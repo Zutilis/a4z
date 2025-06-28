cfa124b71ddd4fb42eb88a9f63a3f0becc024f89
jules ducrot <jules1.ducrot@gmail.com>
2025-06-28 15:54:35 +0200

    landing image optimized

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,17 @@
 <template>
 	<section class="landing-section">
-		<svg xmlns="http://www.w3.org/2000/svg" style="display: none" viewBox="0 0 220 220">
-			<filter id="wavy">
-				<feTurbulence type="turbulence" baseFrequency="0.01" numOctaves="8" result="turbulence" />
-				<feDisplacementMap in2="turbulence" in="SourceGraphic" scale="35" xChannelSelector="R" yChannelSelector="G" />
-			</filter>
-		</svg>
+		<div class="landing-items">
+			<div class="event-intro">
+				<Card variant="beige" locked>
+					<p class="pouler-md">12 juillet 2025 - Fontalon - Roanne</p>
+					<p class="kensington-xl">A4Z Trap #2</p>
+				</Card>
+			</div>
 
-		<div class="event-intro" >
-			<Card variant="beige" locked>
-				<p class="pouler-md">12 juillet 2025 - Fontalon - Roanne</p>
-				<p class="kensington-xl">A4Z Trap #2</p>
-			</Card>
-		</div>
-
-		<div class="event-schedule">
-			<ScheduleCard time="17h - 20h" label="Les stands" details="Marques indés – barber – nails – tournoi FIFA – ..." />
-			<ScheduleCard time="20h - 00h" label="La soirée" details="DJ sets rap – Beendo Z – Livaï – rappeurs – ..." />
+			<div class="event-schedule">
+				<ScheduleCard time="17h - 20h" label="Les stands" details="Marques indés – barber – nails – tournoi FIFA – ..." />
+				<ScheduleCard time="20h - 00h" label="La soirée" details="DJ sets rap – Beendo Z – Livaï – rappeurs – ..." />
+			</div>
 		</div>
 	</section>
 
@@ -60,14 +55,17 @@ setHead(
 <style>
 	.landing-section {
 		position: relative;
+		min-height: 100dvh;
+		margin-bottom: 7rem;
+	}
+
+	.landing-items {
 		display: flex;
 		flex-direction: column;
 		align-items: center;
 		justify-content: center;
 		padding: var(--space-y-header) 0;
 		gap: 2rem;
-		min-height: 100dvh;
-		margin-bottom: 7rem;
 		overflow-y: visible;
 	}
 
@@ -77,11 +75,11 @@ setHead(
 		inset: 0;
 		background-image: url('/images/landing.webp');
 		background-size: cover;
-		background-position: center 40%;
+		background-position: center 100%;
 		background-repeat: no-repeat;
-		filter: url(#wavy);
 		mix-blend-mode: luminosity;
 		z-index: 0;
+		height: 99%;
 		pointer-events: none;
 		transform: scale(1.1);
 	}
